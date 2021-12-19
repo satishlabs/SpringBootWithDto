feecea2ae2675ff8453804e3e92d04af1f6bf1f4
Satish Prasad <spd61185@gmail.com>
2021-12-20 05:00:40 +0530

    updated files

--- src/main/java/com/satishlabs/exception/ResourceNotFoundException.java
+++ src/main/java/com/satishlabs/exception/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.satishlabs.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String message;
 
 	public ResourceNotFoundException(String message, String string, long id) {
